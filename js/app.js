f691efae2e688cecdcab47a8410f483192baed92
Fahid Hossain <fahidhossain2021@gmail.com>
2021-09-13 01:52:16 +0600

    column,image & count fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
+  // const url = `https://fakestoreapi.com/products`;
+  const url = `https://raw.githubusercontent.com/biswajitdasme/fakestore/main/db.json?fbclid=IwAR39gblClcw1RoJ4BpoVqGTv3-EV3jcOyod6jQUp5-CEdiXnzOzcDRCpnWE`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
@@ -8,23 +9,22 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
-  const allProducts = products.map((pd) => pd);
-  for (const product of allProducts) {
-    const image = product.images;
-    const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
-    document.getElementById("all-products").appendChild(div);
-  }
+products.forEach(product =>{
+      const image = product.images;
+      const div = document.createElement("div");
+      div.classList.add("product");
+      div.innerHTML = `<div class="single-product">
+        <div>
+      <img class="product-image" src=${product.image}></img>
+        </div>
+        <h3>${product.title}</h3>
+        <p>Category: ${product.category}</p>
+        <h2>Price: $ ${product.price}</h2>
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+        <button id="details-btn" class="btn btn-danger">Details</button></div>
+        `;
+      document.getElementById("all-products").appendChild(div);
+  });
 };
 let count = 0;
 const addToCart = (id, price) => {
